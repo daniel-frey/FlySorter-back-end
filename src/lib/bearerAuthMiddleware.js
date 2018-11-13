7efa97cf39c2be4b7f32846c659595686688d6fc
Tom <tnorth93@gmail.com>
2018-11-13 08:59:42 -0800

    tweaks for login

--- src/lib/bearerAuthMiddleware.js
+++ src/lib/bearerAuthMiddleware.js
@@ -19,13 +19,11 @@ module.exports = (request, response, next) => {
   if (!request.headers.authorization) {
     return next(new HttpError(400, 'AUTH | invalid request'));
   }
-
   const token = request.headers.authorization.split('Bearer ')[1];
-
   if (!token) {
     return next(new HttpError(400, 'AUTH | invalid request'));
   }
-  return promisify(jsonWebToken.verify)(token, process.env.SECRET)
+  return promisify(jsonWebToken.verify)(token, process.env.APP_SECRET)
     .then((decryptedToken) => {
       return Account.findOne({ tokenSeed: decryptedToken.tokenSeed });
     })
